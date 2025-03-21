91dd771615c32925eab868cf855ac448752d6dba
Chaykov <toumarenshi@gmail.com>
2025-03-21 18:22:52 +0100

    Solidny fundament autoryzacji

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,28 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID ?? "",
-      clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
-    }),
-  ],
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
 
-  secret: process.env.AUTH_SECRET,
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+  return {
+    adapter: NeonAdapter(pool),
+    providers: [
+      GitHub({
+        clientId: process.env.AUTH_GITHUB_ID ?? "",
+        clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+      }),
+    ],
+
+    callbacks: {
+      authorized: async ({ auth }) => {
+        return !!auth;
+      },
+    },
+
+    basePath: "/api/auth",
+    secret: process.env.AUTH_SECRET,
+  };
 });
